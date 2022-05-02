21ad1c0f339d76a28e0d46dec94bbfa8c0446890
Matt Moore <mmoore11@zagmail.gonzaga.edu>
2022-05-02 20:30:04 +0000

    Added new utils function, and improved test file

--- test_myclassifiers.py
+++ test_myclassifiers.py
@@ -1,7 +1,7 @@
 ##############################################
 # Programmer: Matthew Moore
 # Class: CptS 322-01, Spring 2022
-# Programming Assignment #4
+# Programming Assignment # 7
 # 2/9/2022
 # 
 # 
@@ -10,14 +10,221 @@
 ##############################################
 
 import numpy as np
+import mysklearn.myutils as myutils
 from scipy import stats
 from sklearn.neighbors import KNeighborsClassifier, kneighbors_graph
-
+from sklearn.linear_model import LinearRegression
 from mysklearn.mysimplelinearregressor import MySimpleLinearRegressor
 from mysklearn.myclassifiers import MySimpleLinearRegressionClassifier,\
     MyKNeighborsClassifier,\
-    MyDummyClassifier
-from sklearn.linear_model import LinearRegression
+    MyDummyClassifier,\
+    MyNaiveBayesClassifier,\
+    MyDecisionTreeClassifier
+
+
+# interview dataset
+interview_header = ["level", "lang", "tweets", "phd", "interviewed_well"]
+interview_table = [
+        ["Senior", "Java", "no", "no", "False"],
+        ["Senior", "Java", "no", "yes", "False"],
+        ["Mid", "Python", "no", "no", "True"],
+        ["Junior", "Python", "no", "no", "True"],
+        ["Junior", "R", "yes", "no", "True"],
+        ["Junior", "R", "yes", "yes", "False"],
+        ["Mid", "R", "yes", "yes", "True"],
+        ["Senior", "Python", "no", "no", "False"],
+        ["Senior", "R", "yes", "no", "True"],
+        ["Junior", "Python", "yes", "no", "True"],
+        ["Senior", "Python", "yes", "yes", "True"],
+        ["Mid", "Python", "no", "yes", "True"],
+        ["Mid", "Java", "yes", "no", "True"],
+        ["Junior", "Python", "no", "yes", "False"]
+    ]
+
+# note: this tree uses the generic "att#" attribute labels because fit() does not and should not accept attribute names
+# note: the attribute values are sorted alphabetically
+interview_tree = \
+        ["Attribute", "att0",
+            ["Value", "Junior", 
+                ["Attribute", "att3",
+                    ["Value", "no", 
+                        ["Leaf", "True", 3, 5]
+                    ],
+                    ["Value", "yes", 
+                        ["Leaf", "False", 2, 5]
+                    ]
+                ]
+            ],
+            ["Value", "Mid",
+                ["Leaf", "True", 4, 14]
+            ],
+            ["Value", "Senior",
+                ["Attribute", "att2",
+                    ["Value", "no",
+                        ["Leaf", "False", 3, 5]
+                    ],
+                    ["Value", "yes",
+                        ["Leaf", "True", 2, 5]
+                    ]
+                ]
+            ]
+        ]
+
+bramer_solution_tree = ['Attribute', 'att0', 
+                            ['Value', 'A', 
+                                ['Attribute', 'att4', 
+                                    ['Value', 'A', 
+                                        ['Leaf', 'FIRST', 5, 14]
+                                    ], 
+                                    ['Value', 'B', 
+                                        ['Attribute', 'att3', 
+                                            ['Value', 'A', 
+                                                ['Attribute', 'att1', 
+                                                    ['Value', 'A', 
+                                                        ['Leaf', 'FIRST', 1, 2]
+                                                    ], 
+                                                    ['Value', 'B', 
+                                                        ['Leaf', 'SECOND', 1, 2]
+                                                    ]
+                                                ]
+                                            ],
+                                            ['Value', 'B', 
+                                                ['Leaf', 'SECOND', 7, 9]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ], 
+                            ['Value', 'B', 
+                                ['Leaf', 'SECOND', 12, 26]
+                            ]
+                        ]
+
+iphone_solution_tree = ['Attribute', 'att0', 
+                            ['Value', 1, 
+                                ['Attribute', 'att1', 
+                                    ['Value', 1, 
+                                        ['Leaf', 'yes', 1, 5]
+                                    ], 
+                                    ['Value', 2, 
+                                        ['Attribute', 'att2', 
+                                            ['Value', 'excellent', 
+                                                ['Leaf', 'yes', 1, 2]
+                                            ],
+                                            ['Value', 'fair', 
+                                                ['Leaf', 'no', 1, 2]
+                                            ]
+                                        ]
+                                    ],
+                                    ['Value', 3, 
+                                        ['Leaf', 'no', 2, 5]
+                                    ]
+                                ]
+                            ],
+                            ['Value', 2, 
+                                ['Attribute', 'att2', 
+                                    ['Value', 'excellent', 
+                                        ['Leaf', 'no', 4, 10]
+                                    ],
+                                    ['Value', 'fair', 
+                                        ['Leaf', 'yes', 6, 10]
+                                    ]
+                                ]
+                            ]
+                        ]
+
+# bramer degrees dataset
+degrees_header = ["SoftEng", "ARIN", "HCI", "CSA", "Project", "Class"]
+degrees_table = [
+        ["A", "B", "A", "B", "B", "SECOND"],
+        ["A", "B", "B", "B", "A", "FIRST"],
+        ["A", "A", "A", "B", "B", "SECOND"],
+        ["B", "A", "A", "B", "B", "SECOND"],
+        ["A", "A", "B", "B", "A", "FIRST"],
+        ["B", "A", "A", "B", "B", "SECOND"],
+        ["A", "B", "B", "B", "B", "SECOND"],
+        ["A", "B", "B", "B", "B", "SECOND"],
+        ["A", "A", "A", "A", "A", "FIRST"],
+        ["B", "A", "A", "B", "B", "SECOND"],
+        ["B", "A", "A", "B", "B", "SECOND"],
+        ["A", "B", "B", "A", "B", "SECOND"],
+        ["B", "B", "B", "B", "A", "SECOND"],
+        ["A", "A", "B", "A", "B", "FIRST"],
+        ["B", "B", "B", "B", "A", "SECOND"],
+        ["A", "A", "B", "B", "B", "SECOND"],
+        ["B", "B", "B", "B", "B", "SECOND"],
+        ["A", "A", "B", "A", "A", "FIRST"],
+        ["B", "B", "B", "A", "A", "SECOND"],
+        ["B", "B", "A", "A", "B", "SECOND"],
+        ["B", "B", "B", "B", "A", "SECOND"],
+        ["B", "A", "B", "A", "B", "SECOND"],
+        ["A", "B", "B", "B", "A", "FIRST"],
+        ["A", "B", "A", "B", "B", "SECOND"],
+        ["B", "A", "B", "B", "B", "SECOND"],
+        ["A", "B", "B", "B", "B", "SECOND"],
+    ]
+
+degrees_tree = [] # TODO: figure out what this is by finishing Bramer 5.5 Self-assessment exercise 1
+
+# in-class Naive Bayes example (lab task #1)
+inclass_example_col_names = ["att1", "att2"]
+X_train_inclass_example = [
+    [1, 5], # yes
+    [2, 6], # yes
+    [1, 5], # no
+    [1, 5], # no
+    [1, 6], # yes
+    [2, 6], # no
+    [1, 5], # yes
+    [1, 6] # yes
+]
+y_train_inclass_example = ["yes", "yes", "no", "no", "yes", "no", "yes", "yes"]
+
+# RQ5 (fake) iPhone purchases dataset
+iphone_col_names = ["standing", "job_status", "credit_rating", "buys_iphone"]
+X_train_iphone = [
+    [1, 3, "fair"],
+    [1, 3, "excellent"],
+    [2, 3, "fair"],
+    [2, 2, "fair"],
+    [2, 1, "fair"],
+    [2, 1, "excellent"],
+    [2, 1, "excellent"],
+    [1, 2, "fair"],
+    [1, 1, "fair"],
+    [2, 2, "fair"],
+    [1, 2, "excellent"],
+    [2, 2, "excellent"],
+    [2, 3, "fair"],
+    [2, 2, "excellent"],
+    [2, 3, "fair"]
+]
+y_train_iphone = ["no","no","yes","yes","yes","no","yes","no","yes","yes","yes","yes","yes","no","yes"]
+
+# Bramer 3.2 train dataset
+time_col_names = ["day", "season", "wind", "rain", "class"]
+time_table = [
+    ["weekday", "spring", "none", "none", "on time"],
+    ["weekday", "winter", "none", "slight", "on time"],
+    ["weekday", "winter", "none", "slight", "on time"],
+    ["weekday", "winter", "high", "heavy", "late"],
+    ["saturday", "summer", "normal", "none", "on time"],
+    ["weekday", "autumn", "normal", "none", "very late"],
+    ["holiday", "summer", "high", "slight", "on time"],
+    ["sunday", "summer", "normal", "none", "on time"],
+    ["weekday", "winter", "high", "heavy", "very late"],
+    ["weekday", "summer", "none", "slight", "on time"],
+    ["saturday", "spring", "high", "heavy", "cancelled"],
+    ["weekday", "summer", "high", "slight", "on time"],
+    ["saturday", "winter", "normal", "none", "late"],
+    ["weekday", "summer", "high", "none", "on time"],
+    ["weekday", "winter", "normal", "heavy", "very late"],
+    ["saturday", "autumn", "high", "slight", "on time"],
+    ["weekday", "autumn", "none", "heavy", "on time"],
+    ["holiday", "spring", "normal", "slight", "on time"],
+    ["weekday", "spring", "normal", "none", "on time"],
+    ["weekday", "spring", "normal", "slight", "on time"]
+] # 20 newsgroups dataset
 
 # 4 instance table
 header4 = ["Acid Durability","Strength","Classification"]
@@ -252,3 +459,90 @@ def test_dummy_classifier_predict():
                          1000, replace=True, p=[0.09,0.09,0.09,0.09,0.09,0.09,0.09,0.09,0.09,0.19]))
     test_dummy.fit(X_train,y_train)
     assert test_dummy.predict([[1.0]])  == ['j']
+
+
+def test_naive_bayes_classifier_fit():
+    # TEST 1: 8 instance training set example traced in class on the iPad
+    # asserting against our desk check of the priors and posteriors
+    # all desk checks have a clear alphabetical order providing easy comparison
+    correct_priors = [3/8,5/8]
+    correct_posteriors = [["class",1,2,5,6],["no",2/3,1/3,2/3,1/3],["yes",4/5,1/5,2/5,3/5]]
+    mynbc = MyNaiveBayesClassifier()
+    mynbc.fit(X_train_inclass_example,y_train_inclass_example)
+    assert mynbc.priors == correct_priors
+    assert mynbc.posteriors == correct_posteriors
+    # TEST 2: Use the 15 instance training set example from RQ5
+    # asserting against your desk check of the priors and posteriors
+    correct_priors = [5/15,10/15]
+    correct_posteriors = [["class",1,2,1,2,3,"excellent","fair"],
+                          ["no",3/5,2/5,1/5,2/5,2/5,3/5,2/5],
+                          ["yes",2/10,8/10,3/10,4/10,3/10,3/10,7/10]]
+    mynbc.fit(X_train_iphone,y_train_iphone)
+    assert mynbc.priors == correct_priors
+    assert mynbc.posteriors == correct_posteriors
+    # TEST 3: Use Bramer 3.2 Figure 3.1 train dataset example
+    # asserting against the priors and posteriors solution in Figure 3.2.
+    correct_priors = [1/20,2/20,14/20,3/20]
+    correct_posteriors = [["class","holiday","saturday","sunday","weekday","autumn","spring","summer",
+                        "winter","high","none","normal","heavy","none","slight"],
+                       ["cancelled",0/1,1/1,0/1,0/1,0/1,1/1,0/1,0/1,1/1,0/1,0/1,1/1,0/1,0/1],
+                            ["late",0/2,1/2,0/2,1/2,0/2,0/2,0/2,2/2,1/2,0/2,1/2,1/2,1/2,0/2],
+                         ["on time",2/14,2/14,1/14,9/14,2/14,4/14,6/14,2/14,4/14,5/14,5/14,1/14,5/14,8/14],
+                       ["very late",0/3,0/3,0/3,3/3,1/3,0/3,0/3,2/3,1/3,0/3,2/3,2/3,1/3,0/3]]
+    mynbc.fit(myutils.table_by_columns(time_table,time_col_names,time_col_names[:-1]),
+                myutils.get_column(time_table,time_col_names,time_col_names[-1]))
+    assert mynbc.priors == correct_priors
+    assert mynbc.posteriors == correct_posteriors
+
+def test_naive_bayes_classifier_predict():
+    # TEST 1: Use the 8 instance training set example traced in class on the iPad
+    # asserting against our desk check prediction
+    X_test_inclass_example = [[1,5]]
+    mynbc = MyNaiveBayesClassifier()
+    mynbc.fit(X_train_inclass_example,y_train_inclass_example)
+    assert mynbc.predict(X_test_inclass_example) == ["yes"]
+    # TEST 2: Use the 15 instance training set example from RQ5
+    # asserting against your desk check predictions for the two test instances
+    X_test_iphone = [[2,2,"fair"],[1,1,"excellent"]]
+    mynbc.fit(X_train_iphone,y_train_iphone)
+    assert mynbc.predict(X_test_iphone) == ["yes","no"]
+    # TEST 3: Use Bramer 3.2 unseen instance ["weekday", "winter", "high", "heavy"],
+    # and Bramer 3.6 Self-assessment exercise 1 unseen instances
+    # asserting against the solution prediction on pg. 28-29
+    # and the exercise solution predictions in Bramer Appendix E
+    X_test_time = [["weekday","winter","high","heavy"]]
+    mynbc.fit(myutils.table_by_columns(time_table,time_col_names,time_col_names[:-1]),
+                myutils.get_column(time_table,time_col_names,time_col_names[-1]))
+    assert mynbc.predict(X_test_time) == ["very late"]
+
+def test_decision_tree_classifier_fit():
+    # TEST 1: Use the 14 instance "interview" training set example traced in class on the iPad
+    mydecision_tree = MyDecisionTreeClassifier()
+    mydecision_tree.fit(myutils.table_by_columns(interview_table,interview_header,interview_header[:-1]),
+                            myutils.get_column(interview_table,interview_header,interview_header[-1]))
+    assert mydecision_tree.tree == interview_tree
+    # TEST 2: Use the Bramer 4.1 Figure 4.3 degrees dataset example, 
+    # asserting against the tree you create when you finish Bramer 5.5 Self-assessment exercise 1
+    mydecision_tree.fit(myutils.table_by_columns(degrees_table,degrees_header,degrees_header[:-1]),
+                            myutils.get_column(degrees_table,degrees_header,degrees_header[-1]))
+    assert mydecision_tree.tree == bramer_solution_tree
+    # TEST3: Use the 15 instance "iPhone" training set example from RQ5
+    mydecision_tree.fit(X_train_iphone,y_train_iphone)
+    assert mydecision_tree.tree == iphone_solution_tree
+def test_decision_tree_classifier_predict():
+    X_test_interview = [["Junior", "Java", "yes", "no"],["Junior", "Java", "yes", "yes"]]
+    X_test_bramer = [["B", "B", "B", "B", "B"], ["A", "A", "A", "A", "A"], ["A", "A", "A", "A", "B"]]
+    X_test_iphone = [[2,2,"fair"],[1,1,"excellent"]]
+    # TEST 1: Use the 14 instance "interview" training set example traced in class on the iPad
+    mydecision_tree = MyDecisionTreeClassifier()
+    mydecision_tree.fit(myutils.table_by_columns(interview_table,interview_header,interview_header[:-1]),
+                            myutils.get_column(interview_table,interview_header,interview_header[-1]))
+    assert mydecision_tree.predict(X_test_interview) == ["True","False"]
+    # TEST 2: Use the Bramer 4.1 Figure 4.3 degrees dataset example, 
+    # asserting against the tree you create when you finish Bramer 5.5 Self-assessment exercise 1
+    mydecision_tree.fit(myutils.table_by_columns(degrees_table,degrees_header,degrees_header[:-1]),
+                            myutils.get_column(degrees_table,degrees_header,degrees_header[-1]))
+    assert mydecision_tree.predict(X_test_bramer) == ["SECOND","FIRST","FIRST"]
+    # TEST3: Use the 15 instance "iPhone" training set example from RQ5
+    mydecision_tree.fit(X_train_iphone,y_train_iphone)
+    assert mydecision_tree.predict(X_test_iphone) == ["yes","yes"]
